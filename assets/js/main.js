76f80a5dc2c2b8b216f3c9bbedc1ee05357d66d6
52bp <ybelovep@gmail.com>
2020-03-30 18:25:07 +0800

    添加百度统计代码

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,3 +105,11 @@
     },
     600);
 }
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?59e7ec76eade3186dc9345ec66626398";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
